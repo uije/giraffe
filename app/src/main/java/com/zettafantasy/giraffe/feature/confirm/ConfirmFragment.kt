b7b4b312cb4e50bb8fbdb56c97024fe388802ae6
uije.park <uije.park@navercorp.com>
2021-01-08 17:10:02 +0900

    기록 저장 작업중

--- app/src/main/java/com/zettafantasy/giraffe/feature/confirm/ConfirmFragment.kt
+++ app/src/main/java/com/zettafantasy/giraffe/feature/confirm/ConfirmFragment.kt
@@ -9,25 +9,32 @@ import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.zettafantasy.giraffe.GiraffeApplication
 import com.zettafantasy.giraffe.R
+import com.zettafantasy.giraffe.data.GiraffeRepository
 import com.zettafantasy.giraffe.model.Emotion
 import com.zettafantasy.giraffe.model.Need
 
 class ConfirmFragment : Fragment() {
+    private lateinit var emotions: List<Emotion>
+    private lateinit var needs: List<Need>
+    private lateinit var repository: GiraffeRepository
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         setHasOptionsMenu(true)
+        repository = (activity?.application as GiraffeApplication).repository
+
         val view = inflater.inflate(R.layout.confirm_fragment, container, false)
         view.findViewById<AppCompatButton>(R.id.btn_save).setOnClickListener {
-            Navigation.findNavController(view)
-                .navigate(R.id.action_confirm_to_history)
+            //repository.insert()
+            navigateRecordScreen(view)
         }
 
-        val emotions =
-            arguments?.getParcelableArrayList<Emotion>(Emotion::class.simpleName)
+        emotions = arguments?.getParcelableArrayList<Emotion>(Emotion::class.simpleName)!!
         emotions?.let {
             view.findViewById<TextView>(R.id.emotions).text = TextUtils.join(", ", it)
         }
@@ -40,5 +47,10 @@ class ConfirmFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun navigateRecordScreen(view: View) {
+        Navigation.findNavController(view)
+            .navigate(R.id.action_confirm_to_record)
     }
 }
