f5a459ee8a2c37fb88cc332ee79fb68e9d21f55b
uije.park <uije.park@navercorp.com>
2021-01-28 18:31:09 +0900

    기록 불러오기

--- app/src/main/java/com/zettafantasy/giraffe/feature/record/RecordFragment.kt
+++ app/src/main/java/com/zettafantasy/giraffe/feature/record/RecordFragment.kt
@@ -1,37 +1,44 @@
 package com.zettafantasy.giraffe.feature.record
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
+import com.zettafantasy.giraffe.GiraffeApplication
 import com.zettafantasy.giraffe.R
-import com.zettafantasy.giraffe.databinding.HistoryFragmentBinding
-import com.zettafantasy.giraffe.feature.main.MainViewModel
+import com.zettafantasy.giraffe.data.GiraffeRepository
+import com.zettafantasy.giraffe.databinding.RecordFragmentBinding
+import com.zettafantasy.giraffe.feature.main.RecordViewModel
+import com.zettafantasy.giraffe.feature.main.RecordViewModelFactory
 
 class RecordFragment : Fragment() {
-    private lateinit var binding: HistoryFragmentBinding
-    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: RecordFragmentBinding
+    private val viewModel: RecordViewModel by viewModels {
+        RecordViewModelFactory((requireActivity().application as GiraffeApplication).repository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.history_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.record_fragment, container, false)
         binding.fab.setOnClickListener {
             Navigation.findNavController(binding.root)
                 .navigate(R.id.action_history_to_emotion_description)
         }
-        return binding.root
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.allRecords.observe(viewLifecycleOwner) { records ->
+            records.let { Log.d(this.javaClass.simpleName, it.size.toString()) }
+        }
+
+        return binding.root
     }
 }
---
+++ app/src/main/java/com/zettafantasy/giraffe/feature/record/RecordViewModel.kt
@@ -0,0 +1,25 @@
+package com.zettafantasy.giraffe.feature.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.zettafantasy.giraffe.data.GiraffeRepository
+import com.zettafantasy.giraffe.data.Record
+
+class RecordViewModel(private val repository: GiraffeRepository) : ViewModel() {
+
+    val allRecords: LiveData<List<Record>> = repository.allRecords.asLiveData()
+
+}
+
+class RecordViewModelFactory(private val repository: GiraffeRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RecordViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return RecordViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
