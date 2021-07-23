74e9798b08b5bc728cb23b4bad225f26e6b27ceb
uije.park <uije.park@navercorp.com>
2021-07-23 18:34:12 +0900

    기록 삭제 기능 작업중

--- app/src/main/java/com/zettafantasy/giraffe/feature/record/RecordFragment.kt
+++ app/src/main/java/com/zettafantasy/giraffe/feature/record/RecordFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.thekhaeng.recyclerviewmargin.LayoutMarginDecoration
@@ -17,6 +19,7 @@ import com.zettafantasy.giraffe.common.AppExecutors
 import com.zettafantasy.giraffe.data.EmotionInventory
 import com.zettafantasy.giraffe.data.NeedInventory
 import com.zettafantasy.giraffe.databinding.RecordFragmentBinding
+
 
 class RecordFragment : Fragment() {
     private lateinit var binding: RecordFragmentBinding
@@ -72,5 +75,31 @@ class RecordFragment : Fragment() {
         binding.recordRv.addItemDecoration(
             LayoutMarginDecoration(resources.getDimensionPixelSize(R.dimen.record_margin))
         )
+
+        val itemTouchHelper = ItemTouchHelper(
+            object : ItemTouchHelper.SimpleCallback(
+                0,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    AlertDialog.Builder(requireContext())
+                        .setMessage("해당 기록을 지우시겠어요?")
+                        .setPositiveButton("네") { _, _ ->
+                            val position = viewHolder.adapterPosition
+                            // Then you can remove this item from the adapter
+                        }
+                        .setNegativeButton("아니오") { _, _ -> adapter.notifyItemChanged(viewHolder.adapterPosition); }
+                        .show()
+                }
+            })
+        itemTouchHelper.attachToRecyclerView(binding.recordRv)
     }
 }
