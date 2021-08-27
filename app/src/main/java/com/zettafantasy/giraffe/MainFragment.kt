d285b71af34ea3344f1e222242b320725a199ac1
uije.park <uije.park@navercorp.com>
2021-08-27 18:19:08 +0900

    메인화면 탭 적용 작업중

--- app/src/main/java/com/zettafantasy/giraffe/MainFragment.kt
+++ app/src/main/java/com/zettafantasy/giraffe/MainFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.zettafantasy.giraffe.databinding.MainFragmentBinding
+import com.zettafantasy.giraffe.feature.record.RecordFragment
 
 class MainFragment : Fragment() {
     private lateinit var binding: MainFragmentBinding
@@ -19,6 +22,21 @@ class MainFragment : Fragment() {
         setHasOptionsMenu(true)
 
         binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+
+        binding.viewPager.adapter = object : FragmentStateAdapter(this) {
+            override fun createFragment(position: Int): Fragment {
+                return RecordFragment()
+            }
+
+            override fun getItemCount(): Int {
+                return 2
+            }
+        }
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = "기록"
+        }.attach()
+
         return binding.root
     }
 }
