80659bbbc66e169cb51ed8d312551ae60776e667
uije.park <uije.park@navercorp.com>
2022-10-12 17:53:08 +0900

    Gradle Version UP 6.5 -> 7.2, Open Source License 메뉴 추가

--- app/src/main/java/com/zettafantasy/giraffe/feature/setting/SettingFragment.kt
+++ app/src/main/java/com/zettafantasy/giraffe/feature/setting/SettingFragment.kt
@@ -1,9 +1,11 @@
 package com.zettafantasy.giraffe.feature.setting
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.zettafantasy.giraffe.R
 import com.zettafantasy.giraffe.common.Preferences
 import com.zettafantasy.giraffe.feature.remind.NotificationHelper
@@ -14,24 +16,39 @@ class SettingFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.preferences, rootKey)
 
         initClearPreference()
+        initNotificationTest()
+        initLicense()
+    }
 
-        val notification: Preference? = this.findPreference("notification")
-        notification?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-            NotificationHelper.notifyRemindAlarm(requireContext())
-            //GiraffeAlarmManager.init(requireContext())
-            true
+    private fun initNotificationTest() {
+        findPreference<Preference?>("notification")?.let {
+            it.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                NotificationHelper.notifyRemindAlarm(requireContext())
+                //todo GiraffeAlarmManager.init(requireContext())
+                true
+            }
+        }
+    }
+
+    private fun initLicense() {
+        findPreference<Preference?>("license")?.let {
+            it.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                startActivity(Intent(requireContext(), OssLicensesMenuActivity::class.java))
+                true
+            }
         }
     }
 
     private fun initClearPreference() {
-        val clear: Preference? = this.findPreference("clear_preference")
-        clear?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-            if (Preferences.clear()) {
-                Toast.makeText(requireContext(), R.string.complete, Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(requireContext(), R.string.fail, Toast.LENGTH_SHORT).show()
+        findPreference<Preference?>("clear_preference")?.let {
+            it.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                if (Preferences.clear()) {
+                    Toast.makeText(requireContext(), R.string.complete, Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(requireContext(), R.string.fail, Toast.LENGTH_SHORT).show()
+                }
+                true
             }
-            true
         }
     }
 }
